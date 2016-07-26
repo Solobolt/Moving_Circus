83ab5074d46da97d0fb0e0acd45f581b8c68142c
Solobolt <solobolt@gmail.com>
2016-07-26 22:29:47 +1000

    Added test Models

--- Moving Circus/Assets/Scripts/CameraFollow.cs
+++ Moving Circus/Assets/Scripts/CameraFollow.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    private GameObject player;
+    private Transform myTransfrom;
+
+    #region CamOffsets
+    private Vector3 camOffset = new Vector3(0,12.0f,-13.0f);
+    private Vector3 maxLimits = new Vector3(10.0f,12.0f,-13.0f);
+    #endregion;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        myTransfrom = this.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        MoveCamera();
 	}
+
+    //makes the camrea follow the player
+    void MoveCamera()
+    {
+        Vector3 tempPos = (player.transform.position + camOffset);
+        if (tempPos.x >= maxLimits.x)
+        {
+            tempPos.x = maxLimits.x;
+        }
+        if (tempPos.x <= -maxLimits.x)
+        {
+            tempPos.x = -maxLimits.x;
+        }
+
+        tempPos.z = maxLimits.z;
+
+
+        myTransfrom.position = tempPos;
+    }
 }
