fa75d28d77c1a5cb3568260a510d78211dc006c3
Solobolt <solobolt@gmail.com>
2016-08-01 03:24:11 +1000

    Added Main Menu

---
+++ Moving Circus/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlayScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
