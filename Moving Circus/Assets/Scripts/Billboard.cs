9701b5a159b28120681d6105b39d4558b4267f16
Solobolt <solobolt@gmail.com>
2016-08-10 14:24:31 +1000

    Edited arrow script

--- Moving Circus/Assets/Scripts/Billboard.cs
+++ Moving Circus/Assets/Scripts/Billboard.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class Billboard : MonoBehaviour {
 
+    private Transform myTransform;
+
 	// Use this for initialization
 	void Start () {
-	
+        myTransform = this.transform;
 	}
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Camera.main.transform.position, Vector3.up);
+        if(Camera.main.orthographic == true)
+        {
+            myTransform.rotation = Camera.main.transform.rotation;
+        }
+        else
+        {
+            transform.LookAt(Camera.main.transform.position, Vector3.up);
+        }
     }
 }
