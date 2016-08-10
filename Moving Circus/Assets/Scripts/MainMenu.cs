06b782b05e6320d269a9ee9cd6c0f34f883c5171
Solobolt <solobolt@gmail.com>
2016-08-10 23:33:48 +1000

    Added main menu Controls

--- Moving Circus/Assets/Scripts/MainMenu.cs
+++ Moving Circus/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
+
+    public Button[] buttons;
+    private int currentButton = 0;
+
+    bool hasDone = false;
 
 	// Use this for initialization
 	void Start () {
@@ -11,8 +17,23 @@ public class MainMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        ControllerInputs();
 	}
+
+    //registers controller input
+    void ControllerInputs()
+    {
+        if(Input.GetAxis("Vertical") > 0.0f)
+        {
+            currentButton = 0;
+            buttons[currentButton].Select();
+        }
+        if (Input.GetAxis("Vertical") < 0.0f)
+        {
+            currentButton = 1;
+            buttons[currentButton].Select();
+        }
+    }
 
     public void PlayScene(string name)
     {
