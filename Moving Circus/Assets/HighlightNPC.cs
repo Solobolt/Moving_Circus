fe3f68f2d133165989264ab66de441f230c63d89
Solobolt <solobolt@gmail.com>
2016-08-15 02:54:39 +1000

    Fixed day one story, failed highlight script

---
+++ Moving Circus/Assets/HighlightNPC.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HighlightNPC : MonoBehaviour {
+
+    public GameObject[] NPCList;
+
+    public GameObject arrow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Highlight(string NPCname)
+    {
+        foreach(GameObject NPC in NPCList)
+        {
+            if(NPC.gameObject.name != NPCname)
+            {
+                if(NPC.gameObject.GetComponentInChildren<Canvas>() != null)
+                {
+                    Destroy(NPC.gameObject.GetComponentInChildren<Canvas>());
+                }
+            }
+            else
+            {
+                Instantiate(arrow,NPC.transform);
+            }
+        }
+    }
+}
